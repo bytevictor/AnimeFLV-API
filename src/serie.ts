4c5b1e31ce99cfd67baccff5be75c00bf2f00f81
ByteVictor <victorgonzarg@gmail.com>
2020-10-12 08:54:35 +0200

    closes #14

--- src/serie.ts
+++ src/serie.ts
@@ -1,12 +1,34 @@
 class Serie {
-    titulo: string;
-    descripcion: string;
-    numero_capitulos: bigint;
+    private _titulo: string;
+    private _descripcion: string;
+    private _numero_capitulos: number = 0;
+    //Array con los links de capitulos que forman la serie
+    private capitulos: string[];
 
-    constructor(titulo: string, descripcion: string, numero_capitulos: bigint){
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.numero_capitulos = numero_capitulos;
+    constructor(titulo: string, descripcion: string, numero_capitulos: number){
+        this._titulo = titulo;
+        this._descripcion = descripcion;
+        this._numero_capitulos = numero_capitulos;
+    }
+
+    get titulo(): string{
+        return this._titulo;
+    }
+
+    get descripcion(): string{
+        return this._descripcion;
+    }
+
+    set descripcion(descripcion: string) {
+        this._descripcion = descripcion;
+    }
+
+    get numero_capitulos(): number{
+        return this._numero_capitulos;
+    }
+
+    anadirCapitulo(){
+        throw new Error("not Implemented");
     }
 
     descargarSerie(){
