b3045c45315258e89c686081a88e2605b835f132
ByteVictor <victorgonzarg@gmail.com>
2020-10-30 22:46:43 +0100

    Eliminada variable inútil Avanza [HU01] #1 y closes #42

--- src/serie.ts
+++ src/serie.ts
@@ -2,7 +2,6 @@
 export default class Serie {
     private _titulo: string;
     private _descripcion: string;
-    private _numero_capitulos: number = 0;
     //Array con los links de capitulos que forman la serie
     private _capitulos = new Map<number, string>();
 
@@ -24,13 +23,12 @@ export default class Serie {
     }
 
     get numero_capitulos(): number{
-        return this._numero_capitulos;
+        return this._capitulos.size
     }
 
     anadirCapitulo(numero_capitulo: number, link_capitulo: string ){
         if( ! this._capitulos.has(numero_capitulo) ){
             this._capitulos.set(numero_capitulo, link_capitulo);
-            this._numero_capitulos++;
         } else {
             throw new Error("Ya existe el capitulo: " + numero_capitulo);
         }
