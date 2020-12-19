56c86fb48f3368174ceb8d42910fcc083b5b40a9
ByteVictor <victorgonzarg@gmail.com>
2020-12-19 02:36:06 +0100

    Test adaptados a las nuevas rutas y funcionando closes #88 y avanza [HU01] #1 [HU03] #3

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -7,7 +7,7 @@ app = require('../src/index.js');
 describe( "Probando creacion de serie", function() {
     it('Deberia devolver un error porque faltan parametros', function (done) {
       request(app)
-       .post('/anadirserie/Fairy%20Tail')
+       .post('/serie/Fairy%20Tail')
        .send({descripcion:'Fairy tail es un gremio de magos'})
        //, link='https://www3.animeflv.net/anime/fairy-tail'})
        .expect(400)
@@ -19,7 +19,7 @@ describe( "Probando creacion de serie", function() {
     });
     it('Deberia devolver un json con la nueva serie', function (done) {
         request(app)
-         .post('/anadirserie/Fairy%20Tail')
+         .post('/serie/Fairy%20Tail')
          .send({descripcion:'Fairy tail es un gremio de magos', link:'https://www3.animeflv.net/anime/fairy-tail'})
          .expect(200)
          .end(function(err, res) {
@@ -31,13 +31,14 @@ describe( "Probando creacion de serie", function() {
     });
 });
 
-describe( "Probando put de capitulo", function() {
+describe( "Probando post de capitulo", function() {
     it('Deberia devolver una confirmacion con la informacion del capitulo', function (done) {
       request(app)
-       .put('/anadircapitulo/Fairy%20Tail/1/animeflv.net.fairytail.1')
+       .post('/capitulo/Fairy%20Tail/1')
+       .send({link:'https://www3.animeflv.net/ver/fairy-tail-1'})
        .expect(200)
        .end(function(err, res) {
-        expect(res.text).equal('{"OK":"1","link":"animeflv.net.fairytail.1"}');
+        expect(res.text).equal('{"OK":"1","link":"https://www3.animeflv.net/ver/fairy-tail-1"}');
         done();
       });
        
@@ -45,10 +46,11 @@ describe( "Probando put de capitulo", function() {
 
     it('Deberia devolver una confirmacion con la informacion de otro capitulo', function (done) {
         request(app)
-         .put('/anadircapitulo/Fairy%20Tail/2/animeflv.net.fairytail.2')
+         .post('/capitulo/Fairy%20Tail/2')
+         .send({link:'https://www3.animeflv.net/ver/fairy-tail-2'})
          .expect(200)
          .end(function(err, res) {
-          expect(res.text).equal('{"OK":"2","link":"animeflv.net.fairytail.2"}');
+          expect(res.text).equal('{"OK":"2","link":"https://www3.animeflv.net/ver/fairy-tail-2"}');
           done();
         });
          
@@ -58,7 +60,7 @@ describe( "Probando put de capitulo", function() {
 describe( "Probando delete de capitulo", function() {
     it('Deberia devolver un mensaje de confirmacion', function (done) {
       request(app)
-       .delete('/borrarcapitulo/Fairy%20Tail/2')
+       .delete('/capitulo/Fairy%20Tail/2')
        .expect(200)
        .end(function(err, res) {
         expect(res.text).equal('{"DELETED":"2"}');
@@ -71,10 +73,10 @@ describe( "Probando delete de capitulo", function() {
 describe( "Probando get de serie", function() {
     it('Deberia devolver la informacion de la serie', function (done) {
       request(app)
-       .get('/getserie/Fairy%20Tail')
+       .get('/serie/Fairy%20Tail')
        .expect(200)
        .end(function(err, res) {
-        expect(res.text).equal('{"_capitulos":"{\\"1\\":\\"animeflv.net.fairytail.1\\"}","_titulo":"Fairy Tail","_descripcion":"Fairy tail es un gremio de magos","_link":"https://www3.animeflv.net/anime/fairy-tail"}');
+        expect(res.text).equal('{"_capitulos":"{\\"1\\":\\"https://www3.animeflv.net/ver/fairy-tail-1\\"}","_titulo":"Fairy Tail","_descripcion":"Fairy tail es un gremio de magos","_link":"https://www3.animeflv.net/anime/fairy-tail"}');
         done();
       });
        
@@ -84,7 +86,7 @@ describe( "Probando get de serie", function() {
 describe( "Probando delete de serie", function() {
     it('Deberia devolver un mensaje de confirmacion', function (done) {
       request(app)
-       .delete('/borrarserie/Fairy%20Tail')
+       .delete('/serie/Fairy%20Tail')
        .expect(200)
        .end(function(err, res) {
         expect(res.text).equal('{"DELETED":"Fairy Tail"}');
